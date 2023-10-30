fa18e3af6ed67ab831170f7379284a0dbfe68fb9
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2023-10-30 16:59:03 +0100

    Rendering cart and products from header

--- app/page.js
+++ app/page.js
@@ -6,8 +6,8 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
-
   const [cart, setCart] = useState([]);
+  const [page, setPage] = useState("products");
 
   useEffect(() => {
     axios.get("http://127.0.0.1:8000/api/products").then((res) => {
@@ -19,14 +19,12 @@ export default function Home() {
     setCart([...cart, product]);
   };
 
-  return (
-    <main>
-      <header class="bg-gray-700 h-16 flex flex-row">
-        <h1 class="text-2xl  ml-auto p-4">Products</h1>
-        <h1 class="text-2xl mr-auto p-4 flex flex-row">
-          Cart {cart.length > 0 && <h1>({cart.length})</h1>}
-        </h1>
-      </header>
+  const pageSetter = (page) => {
+    setPage(page);
+  };
+
+  const renderProducts = () => (
+    <>
       <div class="body w-3/4 ml-auto mr-auto">
         <div class="grid grid-cols-3 gap-4 pt-5">
           {products.map((product, key) => {
@@ -50,6 +48,55 @@ export default function Home() {
           })}
         </div>
       </div>
+    </>
+  );
+
+  const renderCart = () => (
+    <>
+      <div class="body w-3/4 ml-auto mr-auto">
+        <div class="grid grid-cols-3 gap-4 pt-5">
+          {cart.map((product, key) => {
+            return (
+              <div
+                key={key}
+                class="flex flex-col border border-gray-500 rounded-lg bg-gray-600"
+              >
+                <h1 class="text-center text-lg">
+                  {product.title} - ${product.price}
+                </h1>
+                <img src={product.image} alt="image" class="p-3 pt-2"></img>
+                <button
+                  onClick={() => removeFromCart(product)}
+                  class="place-self-center p-3 mb-3 bg-blue-600 bg-opacity-75 border border-blue-700 rounded-xl"
+                >
+                  Remove
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+
+  return (
+    <main>
+      <header class="bg-gray-700 h-16 flex flex-row">
+        <button
+          onClick={() => pageSetter("products")}
+          class="text-2xl  ml-auto p-4"
+        >
+          Products
+        </button>
+        <button
+          onClick={() => pageSetter("cart")}
+          class="text-2xl mr-auto p-4 flex flex-row"
+        >
+          Cart {cart.length > 0 && <h1>({cart.length})</h1>}
+        </button>
+      </header>
+      {page === "products" && renderProducts()}
+      {page === "cart" && renderCart()}
     </main>
   );
 }
