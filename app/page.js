e662300758b5e21f12f877b6803ca7c7bcfcec5b
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2023-10-30 19:23:42 +0100

    Remove Items and Total Cost + User Friendly user listing

--- app/page.js
+++ app/page.js
@@ -19,12 +19,72 @@ export default function Home() {
   }, []);
 
   const addToCart = (product) => {
-    setCart([...cart, product]);
+    setCart([...cart, { ...product }]);
+  };
+  const removeFromCart = (productToRemove) => {
+    setCart(cart.filter((product) => product !== productToRemove));
   };
 
   const navigateTo = (nextPage) => {
     setPage(nextPage);
   };
+
+  const getTotalSum = () => {
+    let number = cart.reduce((sum, { price }) => sum + price, 0);
+    let roundedNumber = Math.round(number * 100) / 100;
+    return roundedNumber;
+  };
+
+  const renderCart = () => (
+    <>
+      <div class="body w-3/4 ml-auto mr-auto">
+        <h1 class="text-4xl text-center mt-5">Cart</h1>
+        <div class="flex flex-col mt-4">
+          {cart.length > 0 &&
+            cart.map((product, key) => {
+              return (
+                <div
+                  key={key}
+                  class="flex flex-row border border-gray-500 rounded-lg bg-gray-600 h-20 mt-1.5"
+                >
+                  <img src={product.image} alt="image" class="p-1"></img>
+
+                  <h1 class="text-center text-2xl mt-auto mb-auto ml-2">
+                    {product.title}
+                  </h1>
+                  <h1 class="text-2xl mt-auto mb-auto ml-auto mr-3">
+                    ${product.price}
+                  </h1>
+                  <button
+                    onClick={() => removeFromCart(product)}
+                    class="place-self-center p-4 mr-3 bg-blue-600 bg-opacity-75 border border-blue-700 rounded-xl"
+                  >
+                    Remove
+                  </button>
+                </div>
+              );
+            })}
+        </div>
+        {cart.length === 0 && (
+          <>
+            <h1 class="text-4xl ml-auto mr-auto text-center">
+              No items in cart
+            </h1>
+            <h2 class="text-center mt-2 text-lg">
+              Please return to the product page and add some into your cart
+            </h2>
+          </>
+        )}
+      </div>
+      {cart.length > 0 && (
+        <div class="w-3/4 ml-auto mr-auto flex">
+          <h1 class="ml-auto text-xl mt-1 mr-1">
+            Total Cost: ${getTotalSum()}
+          </h1>
+        </div>
+      )}
+    </>
+  );
 
   const renderProducts = () => (
     <>
@@ -50,45 +110,6 @@ export default function Home() {
             );
           })}
         </div>
-      </div>
-    </>
-  );
-
-  const renderCart = () => (
-    <>
-      <div class="body w-3/4 ml-auto mr-auto">
-        <div class="grid grid-cols-3 gap-4 pt-5">
-          {cart.length > 0 &&
-            cart.map((product, key) => {
-              return (
-                <div
-                  key={key}
-                  class="flex flex-col border border-gray-500 rounded-lg bg-gray-600"
-                >
-                  <h1 class="text-center text-lg">
-                    {product.title} - ${product.price}
-                  </h1>
-                  <img src={product.image} alt="image" class="p-3 pt-2"></img>
-                  <button
-                    onClick={() => removeFromCart(product)}
-                    class="place-self-center p-3 mb-3 bg-blue-600 bg-opacity-75 border border-blue-700 rounded-xl"
-                  >
-                    Remove
-                  </button>
-                </div>
-              );
-            })}
-        </div>
-        {cart.length === 0 && (
-          <>
-            <h1 class="text-4xl ml-auto mr-auto text-center">
-              No items in cart
-            </h1>
-            <h2 class="text-center mt-2 text-lg">
-              Please return to the product page and add some into your cart
-            </h2>
-          </>
-        )}
       </div>
     </>
   );
